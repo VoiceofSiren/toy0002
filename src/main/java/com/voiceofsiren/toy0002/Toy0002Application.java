d3b4cfcae049930e366c74d72c0f93f004e606d8
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-03-03 21:43:49 +0900

    feat: create develop branch

---
+++ src/main/java/com/voiceofsiren/toy0002/Toy0002Application.java
@@ -0,0 +1,13 @@
+package com.voiceofsiren.toy0002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy0002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toy0002Application.class, args);
+	}
+
+}
