83d1c437a73b2798c72c474a44e1518d9b1b971e
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-03-19 14:02:24 +0900

    feat: HttpSecurity http settings, fix: /account/logout

--- src/main/java/com/voiceofsiren/toy0002/config/WebSecurityConfig.java
+++ src/main/java/com/voiceofsiren/toy0002/config/WebSecurityConfig.java
@@ -7,7 +7,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -21,17 +25,38 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        /*
+        http    // CSRF (Cross-Site Request Forgery): 사용자가 원하지 않아도 서버 측으로 위조된 요청을 강제로 보내는 해킹 방법
+                .csrf(AbstractHttpConfigurer::disable);     // 배포 시 enable시켜야 함.
+        */
+
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/", "/account/register","/css/**", "/api/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                        .loginPage("/account/login")    //login 시 redirect 경로
+                        .loginPage("/account/login")    // login 시 redirect 경로
                         .permitAll()
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/")          // logout 시 redirect 경로
+                        .permitAll());
+
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1)     // 하나의 id로 동시 접속할 수 있는 최대 세션 수를 지정 (다중 로그인 설정)
+                        .maxSessionsPreventsLogin(true));   // 다중 로그인 허용 개수를 초과하였을 경우
+                                                            //      true: 새로운 로그인을 차단
+                                                            //      false: 기존의 세션을 하나씩 삭제
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionFixation()      // 세션 고정 공격 보호를 위한 로그인 성공 시 세션 설정 방법
+                                .changeSessionId());        // changeSessionId(): 로그인 시 동일한 세션에 대한 id를 변경
+                                                            // newSession(): 로그인 시 새로운 세션을 생성
+                                                            // none(): 로그인 시 세션 정보를 변경하지 않음
 
         return http.build();
     }
@@ -60,16 +85,15 @@ public class WebSecurityConfig {
 
 
     /**
-     * Deprecated
+     *  In-Memory 방식으로 데이터를 저장
      */
     /*
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
+        UserDetails user = User.builder()
+                        .username("admin1")
+                        .password(bCryptPasswordEncoder().encode("1234"))
+                        .roles("ROLE_ADMIN")
                         .build();
 
         return new InMemoryUserDetailsManager(user);
--- src/main/java/com/voiceofsiren/toy0002/controller/AccountController.java
+++ src/main/java/com/voiceofsiren/toy0002/controller/AccountController.java
@@ -2,7 +2,12 @@ package com.voiceofsiren.toy0002.controller;
 
 import com.voiceofsiren.toy0002.dto.UserDTO;
 import com.voiceofsiren.toy0002.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +33,15 @@ public class AccountController {
     @PostMapping("/register")
     public String register(UserDTO userDTO) {
         userService.save(userDTO);
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
         return "redirect:/";
     }
 }
